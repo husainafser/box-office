c22f6b2db194bc33e3b3f1027fda11780a9139cc
Husain <husainafser1@gmail.com>
2022-09-23 13:53:58 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-
 import {Switch,Route} from "react-router-dom";
+import Navs  from "./components/Navs";
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
 function App() {
   return (
+    <div>
+      <Navs />
     <Switch>
-      <Route exact path="/">hhusain</Route>
-      <Route exact path="/starred">starred</Route>
+      <Route exact path="/"><Home /></Route>
+      <Route exact path="/starred"><Starred /></Route>
       <Route>This is 404 Page</Route>
     </Switch>
+    </div>
   );
 }
 
---
+++ src/Pages/Starred.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Starred = () => {
+  return (
+    <div>
+      this is starred page
+    </div>
+  )
+}
+
+export default Starred
