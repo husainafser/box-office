370d278852c17a619865679c91e55ade223fd784
Husain <husainafser1@gmail.com>
2022-09-24 22:46:32 +0530

    Api Fetched

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout';
 const Home = () => {
-  return <MainPageLayout>This is home page</MainPageLayout>;
+  const [input, setInput]=useState('');
+  
+  const onSearch = () =>{
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(
+      result =>{console.log(result);}
+    )
+  };
+
+  const onInputChange = ev =>{
+    setInput(ev.target.value);
+  };
+  const onInputKeyDown=(ev)=>{
+    if(ev.keyCode === 13){
+      onSearch();
+    }
+  }
+  
+  return <MainPageLayout>
+    <input type="text" onKeyDown={onInputKeyDown} onChange={onInputChange} value={input} />
+    <button type='button' onClick={onSearch}>Search</button>
+  </MainPageLayout>;
 }
 
 export default Home;
