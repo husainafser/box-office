998eabf22d9f2cbd5e2fccd36a341aac95d0bd4d
Husain <husainafser1@gmail.com>
2022-09-26 15:55:31 +0530

    encapsulated API

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from "../misc/config";
 const Home = () => {
   const [input, setInput]=useState('');
+  const [results, setResults]=useState(null);
   
   const onSearch = () =>{
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(
-      result =>{console.log(result);}
+    apiGet(`/search/shows?q=${input}`).then(
+      result =>{setResults(result);}
     )
   };
 
@@ -17,10 +19,22 @@ const Home = () => {
       onSearch();
     }
   }
-  
+  const renderResults = ()=>{
+       if (results && results.length === 0) {
+            return <div>No Results Found !</div>
+       }
+
+       if (results && results.length > 0) {
+        return <div>{results.map( (item)=> <div key={item.show.id}>{item.show.name}</div> )}</div>
+      }
+
+      return null;
+  }
+
   return <MainPageLayout>
     <input type="text" onKeyDown={onInputKeyDown} onChange={onInputChange} value={input} />
     <button type='button' onClick={onSearch}>Search</button>
+    {renderResults()}
   </MainPageLayout>;
 }
 
