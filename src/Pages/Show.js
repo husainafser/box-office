1ab5757ea1e29df7a073b2af7d3963d016a2770a
Husain <husainafser1@gmail.com>
2022-10-15 19:42:13 +0530

    error solved

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,17 +1,48 @@
- import React, { useEffect, useState } from 'react';
+ import React from 'react';
+ import {useState} from 'react';
+ import {useEffect} from 'react';
  import { useParams } from "react-router-dom";
 import { apiGet } from '../misc/config';
- const show = () => {
+
+ const Show = () => {
     const {id} = useParams();
 
     const [show, setshow] = useState(null);
+    const [isLoading, setIsLoading]=useState(true);
+    const [error, setError] = useState(null);
 
+    let isMounted = true;
     useEffect( ()=>{
          apiGet(`/shows/${id}?embell[]=seasons&embed[]=cast`).then(results=>{
+            if(isMounted){ 
             setshow(results);
-         })
+            setIsLoading(false)
+            }
+         }).catch(err =>{
+          if(isMounted){ 
+            setError(err.message);
+            setIsLoading(false);
+          }
+         });
+         return()=>{
+          isMounted=false;
+         }
     }, [id])
     console.log('show',show); 
+    if (isLoading) {
+        return (
+            <div>
+              Data is being loading
+            </div>
+          )
+    }
+    if (error) {
+        return (
+            <div>
+              Error occured: {error}
+            </div>
+          )
+    }
    return (
      <div>
        this show page
@@ -19,5 +50,5 @@ import { apiGet } from '../misc/config';
    )
  }
  
- export default show
- 
+ export default Show
+   
